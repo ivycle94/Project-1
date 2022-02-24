1fb0215db145b588f6e45bd5b65eb82e7166a13c
Ivycle94 <Ivycle94@gmail.com>
2022-02-23 19:55:57 -0600

    refactored class constructors

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // console.log("test")
 // grabbing html elemnts and setting them up as variables to manipulate
 const game = document.getElementById("canvas")
-
 // getting game's context, which allows to specify where to put things and how big to make them.
 const ctx = game.getContext("2d")
+// declare obstacles as a variable for an array
 
 // now you can set attributes to the game,
 // to set heigh and width based on COMPUTED STYLE
@@ -14,109 +14,152 @@ game.setAttribute("height", getComputedStyle(game)["height"])
 //*********************** Class Constructor for Ball *******************************
 
 class Ball {
-    constructor(x, y, color, height, width) {
-        this.x = x,
-        this.y = y,
-        this.color = color,
-        this.height = height,
-        this.width = width,
-        // this.speedX = 0,
-        this.speedY = 0,
-        this.gravity = 0,
-        this.gravitySpeed = 3,
-        this.alive = true,
-        this.render = function () {
-            ctx.fillStyle = this.color
-            ctx.fillRect(this.x, this.y, this.height, this.width)
-        },
-        // this.update = function () {
-        //     ctx = game
-        //     ctx.fillStyle = this.color
-        //     ctx.fillRect(this.x, this.y, this.height, this.width)
-        // }
-        this.newPos = function () {
-            if(this.y > canvas.height - this.height) {
-              this.y = canvas.height - this.height
-              this.vy = 0
-            }
-            this.gravitySpeed += this.gravity
-            // this.x += this.speedX
-            this.y += this.speedY + this.gravitySpeed
+    constructor () {
+        this.x = 150,
+        this.y = 200,
+        this.vy = 0,
+        this.width = 40,
+        this.height = 40,
+        this.color = "black",
+        this.weight = 1,
+        this.alive = true
+    }
+    update = function () {
+        if(this.y > canvas.height - this.height) {
+            //bottom barrier
+            this.y = canvas.height - this.height
+            this.vy = 0
+        } else {
+            // add gravity to pull player down
+            this.vy += this.weight
+            this.y += this.vy
+            // friction effect below
+            this.vy *= 0.9
         }
+        if(this.y < 0) {
+            //top barrier
+            this.y = 0,
+            this.vy = 0
+        }
+        if (pressedJump === true) {
+            this.jump()
+        }   
+    }
+    render = function () {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+    jump = function () {
+        this.vy -= 2
     }
 }
+let player = new Ball()
+// let ball = new Ball(40, 200, "black", 40, 40)
 
 //******************* Class Constructor for Obstacles ***************************
+
 //need new wall to appear every so often
 //wall should appear a end of canvas <--- done
 //should work its way to 0 x coordinate
+const obstacles = []
 
-
-// should be singular (below)
-class Obstacles {
-    constructor(x, y, color, height, width) {
-        this.x = x,
-        this.y = y,
-        this.color = color,
-        this.height = height,
-        this.width = width,
-        this.alive = true,
-        this.moveObstacles = function () {
-            if (this.x < 0) {
-                this.x -= 1
-            }
-        },
-        this.render = function () {
-            ctx.fillStyle = this.color
-            ctx.fillRect(this.x, this.y, this.height, this.width)
-        }
-    } 
+class Obstacle {
+    constructor () {
+        this.topObstacles = (Math.floor(Math.random() * canvas.height/3)) + 60,
+        this.bottomObstacles = (Math.floor(Math.random() * canvas.height/3)) + 70,
+        this.x = canvas.width,
+        this.width = 100,
+        this.color = "white"
+    }
+    render = function () {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, 0, this.width, this.topObstacles) // <----------------------------------------- top walls
+        ctx.fillRect(this.x, canvas.height - this.bottomObstacles, this.width, this.bottomObstacles) // <--- bottom walls
+    }
+    update = function () {
+        this.x -= 1
+        this.render()
+    }
 }
+
+const spawnObstacles = () => {
+    // 10 frames per wall 
+    if (frame % 500 === 0) {
+        obstacles.push(new Obstacle)
+        //add more speed after each spawn
+        // gamespeed ++ <-- doesn't work.
+    }
+    for (let i = 0; i < obstacles.length; i++){
+        obstacles[i].update()
+    }
+    // Laptop crashed, must get rid of extra obstacles
+    if (obstacles.length > 10) {
+        obstacles.pop()
+    }
+}
+// DOESN"T WORK
+//  const spawnObstacles = () => {
+//             if (obstacle.x < 800) {
+//             let obstacle2 = new Obstacle(920, 250, "white", 50, 250)
+//             obstacles.push(obstacle2)
+//             obstacle2.render()
+//             let obstacle3 = new Obstacle(920, 250, "white", 50, 250) 
+//             obstacles.push(obstacle3) 
+//             obstacle3.render()
+//             }  
+//     }
 
 //******************* Class Constructor for Stars ***************************
+const stars = []
 
 class Star {
-    constructor(x, y, color, height, width) {
-        this.x = x,
-        this.y = y,
-        this.color = color,
-        this.height = height,
-        this.width = width,
-        this.alive = true,
-        this.render = function () {
-            ctx.fillStyle = this.color
-            ctx.fillRect(this.x, this.y, this.height, this.width)
-        }
+    constructor() {
+        this.x = 480,
+        this.y = (Math.floor(Math.random() * canvas.height)),
+        this.color = "yellow",
+        this.height = 20,
+        this.width = 20,
+        this.alive = true  
+    }
+    render = function () {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.height, this.width)
+    }
+    update = function () {
+        this.x -= 1
+        this.render()
     }
 }
 
 
-let ball = new Ball(40, 200, "black", 46, 46) // <---- correct placement
-// let ball = new Ball(480, 100, "black", 46, 46) // <-------detectHit (test) placement
-let obstacles = new Obstacles(920, 250, "white", 50, 250)
-let star = new Star(40, 40, "yellow", 20, 20)
-
-// let frame = 0
+const spawnStars = () => {
+    if (frame % 400 === 0) {
+        stars.unshift(new Star)
+    }
+    for (let i = 0; i < stars.length; i++){
+        stars[i].update()
+    }
+    // Laptop crashed, must get rid of extra obstacles
+    if (stars.length > 1) {
+        stars.pop(stars[0])
+    }
+}
+// let star = new Star(40, 40, "yellow", 20, 20)
+let pressedJump = false
+let frame = 0
+let gamespeed = 2
 
 const gameLoop = () => {
-
-    // the "win condition is to collect  # of stars"
-    // don't touch the obastacles <-- collision detection
-    // otherwise game will end 
-    if (ball.alive) {
-        
-    }
     ctx.clearRect(0, 0, game.width, game.height)
-    obstacles.render()
-    obstacles.moveObstacles()
-    star.render() 
-    ball.render()
-    ball.newPos()
-    ball.update()
-    // requestAnimationFrame(gameLoop)   
+    player.update()
+    player.render()
+    spawnStars()
+    spawnObstacles()
+    requestAnimationFrame(gameLoop)  
+    frame ++ 
+    // console.log(frame)
 }
-
-
+gameLoop();
 
 //******************* Collision Detection ***************************
 
@@ -126,20 +169,39 @@ const detectHit = () => {
     // }
 }
 
+//******************* Collect Detection ***************************
 
-const jump = (e) => {
-    // spacebar keycode ----> 32
-    switch (e.keyCode) {
-        case (32):
-            ball.y -= 60
-            break
-    }
+const detectCollect = () => {
+    
 }
+
+addEventListener("keydown", function(e){
+    console.log("you pressed", e.code)
+    if (e.code === "Space") {
+        // player.y -= 50
+        pressedJump = true
+    }
+})
+addEventListener("keyup", function(e){
+    console.log("you released", e.code)
+    if (e.code === "Space") {
+        pressedJump = false
+    }
+})
+
+// const jump = (e) => {
+//     // spacebar keycode ----> 32
+//     switch (e.keyCode) {
+//         case (32):
+//             ball.y -= 40
+//             break
+//     }
+// }
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
     
-    document.addEventListener("keydown", jump)
-    setInterval(gameLoop, 30)
-
+    // document.addEventListener("keydown", jump)
+    // setInterval(gameLoop, 1000)
 })
